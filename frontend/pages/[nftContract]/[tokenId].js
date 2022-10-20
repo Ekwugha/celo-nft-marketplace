1bce5fbbd5604cf4b1b9fe448b2e6275a9332a0a
Ekwugha <elloekwugha@gmail.com>
2022-10-20 18:51:59 +0100

    added the frontend aspect of the spp

--- frontend/pages/[nftContract]/[tokenId].js
+++ frontend/pages/[nftContract]/[tokenId].js
@@ -11,32 +11,36 @@ import styles from "../../styles/Details.module.css";
 
 
 export default function NFTDetails() {
-    // Extract NFT contract address and token id from url
-    const router = useRouter();
-    const nftAddress = router.query.nftAddress;
-    const tokenId = router.query.tokenId;
+    // Extract NFT contract address and Token ID from URL
+  const router = useRouter();
+  const nftAddress = router.query.nftContract;
+  const tokenId = router.query.tokenId;
 
-    // state variables to contain NFT and listing information
-    const [listing, setListing] = useState();
-    const [name, setName] = useState("");
-    const [imageURI, setImageURI] = useState("");
-    const [isOwner, setIsOwner] = useState(false);
-    const [isActive, setIsActive] = useState(false);
+  // State variables to contain NFT and listing information
+  const [listing, setListing] = useState();
+  const [name, setName] = useState("");
+  const [imageURI, setImageURI] = useState("");
+  const [isOwner, setIsOwner] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
-    // state variables to contain various loading states
-    const [loading, setLoading] = useState(true);
-    const [updating, setUpdating] = useState(false);
-    const [canceling, setCanceling] = useState(false);
-    const [buying, setBuying] = useState(false);
+  // State variable to contain new price if updating listing
+  const [newPrice, setNewPrice] = useState("");
 
-    // fetch signer from wagmi
-    const { data: signer } = useSigner();
+  // State variables to contain various loading states
+  const [loading, setLoading] = useState(true);
+  const [updating, setUpdating] = useState(false);
+  const [canceling, setCanceling] = useState(false);
+  const [buying, setBuying] = useState(false);
 
-    const MarketplaceContract = useContract({
-        addressOrName: MARKETPLACE_ADDRESS,
-        contractInterface: MarketplaceABI,
-        signerOrProvider: signer,
-    });
+  // Fetch signer from wagmi
+  const { data: signer } = useSigner();
+
+  const MarketplaceContract = useContract({
+    address: MARKETPLACE_ADDRESS,
+    abi: MarketplaceABI,
+    signerOrProvider: signer,
+  });
+
 
     async function fetchListing() {
         const listingQuery = `
@@ -80,6 +84,153 @@ export default function NFTDetails() {
     }
 
 
+    // function to fetch NFT details from its metadata, similar to the one in the listing
+    async function fetchNFTDetails() {
+        const ERC721Contract = new Contract(nftAddress, erc721ABI, signer);
+        let tokenURI = await ERC721Contract.tokenURI(tokenId);
+        tokenURI = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/");
+    
+        const metadata = await fetch(tokenURI);
+        const metadataJSON = await metadata.json();
+    
+        let image = metadataJSON.imageUrl;
+        image = image.replace("ipfs://", "https://ipfs.io/ipfs/");
+    
+        setName(metadataJSON.name);
+        setImageURI(image);
+    }
+    
+
+
+    // function to call update listing in the smart contract
+    async function updateListing() {
+        setUpdating(true);
+        const updateTxn = await MarketplaceContract.updateListing(nftAddress, tokenId, parseEther(newPrice));
+        await updateTxn.wait();
+        await fetchListing();
+        setUpdating(false);
+    }
+
+
+    // function to call the 'cancelListing' in the smart contract
+    async function cancelListing() {
+        setCanceling(true);
+        const cancelTxn = await MarketplaceContract.cancelListing(nftAddress, tokenId);
+        await cancelTxn.wait();
+        window.alert("Listing canceled");
+        await router.push("/");
+        setCanceling(false);
+        
+    }
+
+
+    // function to call 'buyListing' in the smart contract
+    async function buyListing() {
+        setBuying(true);
+        const buyTxn = await MarketplaceContract.purchaseListing(nftAddress, tokenId, { value: listing.price });
+        await buyTxn.wait();
+        await fetchListing();
+        setBuying(false);
+    }
+
+
+    // Load listing and NFT data on page load
+    useEffect(() => {
+        if (router.query.nftContract && router.query.tokenId && signer) {
+        Promise.all([fetchListing(), fetchNFTDetails()]).finally(() =>
+            setLoading(false)
+        );
+        }
+    }, [router, signer]);
+
+
+
+    return(
+        <>
+            <Navbar />
+            <div>
+                {loading ? (
+                    <span>Loading...</span>
+                ) : (
+                    <div className={styles.container}>
+                        <div className={styles.details}>
+                            <img src={imageURI} />
+                            <span>
+                                <b>
+                                    {name} - #{tokenId}
+                                </b>
+                            </span>
+                            <span> Price: {formatEther(listing.price)} CELO </span>
+                            <span>
+                                <a href={`https://alfajores.celoscan.io/address/${listing.seller}`} target="_blank">
+                                Seller:{" "}
+                                    {isOwner ? "You" : listing.seller.substring(0, 6) + "..."}
+                                </a>
+                            </span>
+                            <span>Status: {listing.buyer === null ? "Active" : "Sold"}</span>
+                        </div>
+
+                        <div className={styles.options}>
+                            {!isActive && (
+                                <span>
+                                Listing has been sold to{" "}
+                                <a
+                                    href={`https://alfajores.celoscan.io/address/${listing.buyer}`}
+                                    target="_blank"
+                                >
+                                    {listing.buyer}
+                                </a>
+                                </span>
+                            )}
+
+                            {isOwner && isActive && (
+                                <>
+                                <div className={styles.updateListing}>
+                                    <input
+                                    type="text"
+                                    placeholder="New Price (in CELO)"
+                                    value={newPrice}
+                                    onChange={(e) => {
+                                        if (e.target.value === "") {
+                                        setNewPrice("0");
+                                        } else {
+                                        setNewPrice(e.target.value);
+                                        }
+                                    }}
+                                    ></input>
+                                    <button disabled={updating} onClick={updateListing}>
+                                    Update Listing
+                                    </button>
+                                </div>
+
+                                <button
+                                    className={styles.btn}
+                                    disabled={canceling}
+                                    onClick={cancelListing}
+                                >
+                                    Cancel Listing
+                                </button>
+                                </>
+                            )}
+
+                            {!isOwner && isActive && (
+                                <button
+                                className={styles.btn}
+                                disabled={buying}
+                                onClick={buyListing}
+                                >
+                                Buy Listing
+                                </button>
+                            )}
+                        </div>
+                    </div>
+                )}
+            </div>
+        
+        
+        
+        </>
+    )
 
 
 
